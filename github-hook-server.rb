b2cfe2a5bf01846ed9573f27cebf394566cdfbd1
Cliff Rowley <cliff@eleventyten.com>
2008-05-17 12:46:07 -0500

    Totally backtracked on the daemon support.  That were a bit broken.

--- github-hook-server.rb
+++ github-hook-server.rb
@@ -4,61 +4,40 @@ require 'rubygems'
 require 'json'
 require 'sinatra'
 require 'yaml'
-require 'simple-daemon'
 
-# Prevent Sinatra from running until we're ready
-Sinatra::Application.default_options.merge!(:run => false)
-
-# Set Sinatra's working directory
-SimpleDaemon::WORKING_DIRECTORY = "#{File.dirname(__FILE__)}"
-
-class GithubHookServer < SimpleDaemon::Base
-  def initialize
-    # Ignore logout
-    Signal.trap('HUP', 'IGNORE')
-    
+class GithubHookServer
+  def process_payload(json_payload)
     # Load server config
     @config = YAML.load_file('config.yml')
     
     # Set the default template
     @config["template"] ||= "[<%= commit['repo'] %>] <%= commit['url'] %> by <%= commit['author']['name'] %> - <%= commit['message'] %>"
     
-    post '/' do
-      # Quick and dirty sanity check
-      return unless params.keys.include?("payload")
-      
-      # Parse the packet
-      payload = JSON.parse(params[:payload])
+    # Parse the packet
+    payload = JSON.parse(json_payload)
 
-      # Quick and dirty sanity check
-      return unless payload.keys.include?("repository")
+    # Quick and dirty sanity check
+    return unless payload.keys.include?("repository")
 
-      # Grab the repo name
-      repo = payload["repository"]["name"]
+    # Grab the repo name
+    repo = payload["repository"]["name"]
 
-      # Iterate configured hooks and fire appropriate plugins
-      @config[repo]["hooks"].each do |hook_config|
-        # Concat the hook name
-        name = "#{hook_config[0]}_hook"
-        # Concat the hook class name
-        clsn = "#{name}".split('_').map {|w| w.capitalize}.join
-        # Require the appropriate hook
-        require "hooks/#{name}"
-        # Fire!
-        Module.const_get(clsn).new.invoke(payload, hook_config.last, @config)
-      end
-      
-      "OMGLITTLEHORSES! IT WORKED!"
+    # Iterate configured hooks and fire appropriate plugins
+    @config[repo]["hooks"].each do |hook_config|
+      # Concat the hook name
+      name = "#{hook_config[0]}_hook"
+      # Concat the hook class name
+      clsn = "#{name}".split('_').map {|w| w.capitalize}.join
+      # Require the appropriate hook
+      require "hooks/#{name}"
+      # Fire!
+      Module.const_get(clsn).new.invoke(payload, hook_config.last, @config)
     end
-  end
   
-  def self.start
-    Sinatra.run
-  end
-  
-  def self.stop
-    puts "Daemon dying"
+    "OMGLITTLEHORSES! IT WORKED!"
   end
 end
 
-GithubHookServer.daemonize
+post '/' do
+  GithubHookServer.new.process_payload(params['payload']) unless params.keys.include?('payload')
+end
