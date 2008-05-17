cc49649c47e3cd6be48cd1d746caefeee72b481c
Cliff Rowley <cliff@eleventyten.com>
2008-05-17 05:44:21 -0500

    Added sendmail hook

---
+++ hooks/sendmail_hook.rb
@@ -0,0 +1,21 @@
+Dir[File.dirname(__FILE__) + "../vendor/*"].each do |l|
+  $:.unshift "#{File.expand_path(l)}/lib"
+end
+
+require 'simplemail'
+
+class SendmailHook
+  
+  def invoke(payload, hook_config, server_config)
+    mail         = SimpleMail.new
+    mail.to      = hook_config['to']
+    mail.from    = hook_config['from']
+    mail.sender  = hook_config['sender']
+    mail.text    = hook_config['text']
+    mail.html    = hook_config['html']
+    mail.subject = hook_config['subject']
+    
+    mail.send
+  end
+  
+end
