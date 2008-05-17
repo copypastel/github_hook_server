e992162818f74395123edd56c414e7bd46d63ac3
Cliff Rowley <cliff@eleventyten.com>
2008-05-17 05:40:11 -0500

    Modified to make testing easier

--- github-hook-server-test.rb
+++ github-hook-server-test.rb
@@ -3,8 +3,8 @@ require 'net/http'
 payload = %Q{{ 
   "before": "5aef35982fb2d34e9d9d4502f6ede1072793222d",
   "repository": {
-    "url": "http://github.com/cliffrowley/testing_github_hooks",
-    "name": "testing_github_hooks",
+    "url": "http://github.com/cliffrowley/test_repo",
+    "name": "test_repo",
     "owner": {
       "email": "someone@somewhere.com",
       "name": "cliffrowley" 
@@ -12,7 +12,7 @@ payload = %Q{{
   },
   "commits": {
     "41a212ee83ca127e3c8cf465891ab7216a705f59": {
-      "url": "http://github.com/cliffrowley/testing_github_hooks/commit/41a212ee83ca127e3c8cf465891ab7216a705f59",
+      "url": "http://github.com/cliffrowley/test_repo/commit/41a212ee83ca127e3c8cf465891ab7216a705f59",
       "author": {
         "email": "someone@somewhere.com",
         "name": "Cliff Rowley" 
@@ -21,7 +21,7 @@ payload = %Q{{
       "timestamp": "2008-02-15T14:57:17-08:00" 
     },
     "de8251ff97ee194a289832576287d6f8ad74e3d0": {
-      "url": "http://github.com/cliffrowley/testing_github_hooks/commit/de8251ff97ee194a289832576287d6f8ad74e3d0",
+      "url": "http://github.com/cliffrowley/test_repo/commit/de8251ff97ee194a289832576287d6f8ad74e3d0",
       "author": {
         "email": "someone@somewhere.com",
         "name": "Cliff Rowley" 
@@ -33,7 +33,7 @@ payload = %Q{{
   "after": "de8251ff97ee194a289832576287d6f8ad74e3d0" 
 }}
 
-url = URI.parse('http://localhost:8765/')
+url = URI.parse('http://localhost:4567/')
 req = Net::HTTP::Post.new(url.path)
 req.set_form_data({'payload' => payload})
 res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
